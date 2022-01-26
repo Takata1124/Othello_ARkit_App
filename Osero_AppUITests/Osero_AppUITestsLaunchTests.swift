ee6e749cc30ccaa3fa9cb85b7fb261493f374371
takata <skkyosk1124@gmail.com>
2022-01-26 20:02:42 +0900

    Initial Commit

---
+++ Osero_AppUITests/Osero_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Osero_AppUITestsLaunchTests.swift
+//  Osero_AppUITests
+//
+//  Created by t032fj on 2022/01/26.
+//
+
+import XCTest
+
+class Osero_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
